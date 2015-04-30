3afa9ea83b15482287ebebde177b65ac88f0626f
rgoncalves94 <r.goncalves94@hotmail.com>
2015-04-30 18:24:10 -0300

    Configurando Path dos arquivos(BASES DE DADOS)

--- src/controller/ControllerManipuladorArquivo.java
+++ src/controller/ControllerManipuladorArquivo.java
@@ -20,67 +20,68 @@ import java.io.LineNumberReader;
 import java.util.Scanner;
 
 public class ControllerManipuladorArquivo {
-	public ControllerManipuladorArquivo() {
 
-	}
+    public ControllerManipuladorArquivo() {
 
-	static final String PATH = "C:/Users/Rogerio-note/WorkSpace/ProjetoRoteirizacao/Projeto_Roteirizacao/src/arquivos/";
+    }
 
-	public String[] leitor(String file) {
-		String[] vmLinhas;
-		try {
-			vmLinhas = new String[linhas(file)];
+    static final String PATH = "C:/Projetos/roteirizacao/arquivos/src/arquivos/";
 
-			BufferedReader buffRead;
-			buffRead = new BufferedReader(new FileReader(PATH + file));
-			String linha = "";
-			int contador = 0;
-			while (true) {
-				if (linha != null) {
-					linha = buffRead.readLine();
-					vmLinhas[contador] = linha;
+    public String[] leitor(String file) {
+        String[] vmLinhas;
+        try {
+            vmLinhas = new String[linhas(file)];
 
-				} else {
-					break;
-				}
-				contador++;
-			}
-			buffRead.close();
-			return vmLinhas;
-		} catch (FileNotFoundException ex) {
-			System.out.println("1");
-			ex.getStackTrace();
-		} catch (IOException ex) {
-			System.out.println("2");
-			ex.getStackTrace();
-		}
-		return null;
-	}
+            BufferedReader buffRead;
+            buffRead = new BufferedReader(new FileReader(PATH + file));
+            String linha = "";
+            int contador = 0;
+            while (true) {
+                if (linha != null) {
+                    linha = buffRead.readLine();
+                    vmLinhas[contador] = linha;
 
-	public void escritor(String file, String linha) {
-		try {
-			BufferedWriter buffWrite = new BufferedWriter(new FileWriter(PATH
-					+ file, true));
-			buffWrite.append(linha);
-			buffWrite.newLine();
-			buffWrite.close();
+                } else {
+                    break;
+                }
+                contador++;
+            }
+            buffRead.close();
+            return vmLinhas;
+        } catch (FileNotFoundException ex) {
+            System.out.println("1");
+            ex.getStackTrace();
+        } catch (IOException ex) {
+            System.out.println("2");
+            ex.getStackTrace();
+        }
+        return null;
+    }
 
-		} catch (IOException ex) {
-			System.out.println("3");
-			ex.getStackTrace();
-		}
-	}
+    public void escritor(String file, String linha) {
+        try {
+            BufferedWriter buffWrite = new BufferedWriter(new FileWriter(PATH
+                    + file, true));
+            buffWrite.append(linha);
+            buffWrite.newLine();
+            buffWrite.close();
 
-	private int linhas(String file) {
-		try {
-			LineNumberReader lineCounter = new LineNumberReader(new FileReader(
-					PATH + file));
-			return lineCounter.getLineNumber();
-		} catch (FileNotFoundException ex) {
-			System.out.println("4");
-			ex.getStackTrace();
-		}
-		return 0;
-	}
+        } catch (IOException ex) {
+            System.out.println("3");
+            ex.getStackTrace();
+        }
+    }
+
+    private int linhas(String file) {
+        try {
+            LineNumberReader lineCounter = new LineNumberReader(new FileReader(
+                    PATH + file));
+            return lineCounter.getLineNumber();
+        } catch (FileNotFoundException ex) {
+            System.out.println("4");
+            ex.getStackTrace();
+        }
+        return 0;
+    }
 
 }
